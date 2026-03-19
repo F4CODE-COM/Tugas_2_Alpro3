523558487694d93c1a9fd520059a0a9530b6d6ae
F4CODE-COM <febrianalkatammi21@gmail.com>
2026-03-19 11:10:18 +0700

    soalLatihan.cpp

--- soalLatihan.cpp
+++ soalLatihan.cpp
@@ -1,16 +1,52 @@
-/*
-Pak Budi adalah seorang pedagang yang harus mengirim barang setiap hari ke beberapa kota. Dia memiliki tiga pilihan jasa ekspedisi untuk mengirim barang berdasarkan berat dan tujuan pengiriman:
-1. JNE
-- Biaya Rp10.000/kg untuk pengiriman dalam kota.
-- Biaya Rp20.000/kg untuk pengiriman luar kota.
-- Diskon 50% jika berat barang lebih dari 10 kg.
-2. SiCepat
-- Biaya Rp8.000/kg untuk pengiriman dalam kota.
-- Biaya Rp18.000/kg untuk pengiriman luar kota.
-- Gratis ongkir jika berat barang lebih dari 15 kg.
-3. Gojek
-- Hanya bisa mengirim dalam kota dengan biaya Rp12.000/kg.
-- Diskon Rp10.000 jika berat barang lebih dari 5 kg.
-*/
+#include <iostream> //header c++.
+using namespace std; // namespace untuk std input/output.
 
-// Hapus komentar yang berisi soal sebelum memasukkan kode untuk jawaban
+int main() {
+    int tujuan, berat, ekspedisi;
+    long biaya,diskon;
+    system("cls");
+
+    cout << "WELCOME PAK BUDI\n";
+    cout << "Masukkan berat barang (kg): ";
+    cin >> berat;
+    cout << "Masukkan tujuan:dalam kota(1) dan luar kota(2), cukup masukkan angkanya saja : ";
+    cin >> tujuan;
+    cout << "Masukkan Pilihan Ekspedisi (1.JNE 2.SiCepat 3.Gojek, masukkan angkanya saja ): ";
+    cin >> ekspedisi; cout <<"\n\n";
+    //menerima input user
+    switch (ekspedisi){ // menyeleksi ekspedisi pilihan user.
+        case 1 :cout<<"Ekspedisi yang dipilih: JNE\n";
+            if(tujuan==1){
+            biaya=10000*berat;}
+            else if(tujuan==2){
+                biaya=20000*berat;}
+            else{cout<<"Input anda salah!";}
+            if(berat>10){
+                diskon=biaya*0.5;// variabel diskon adalah variabel biaya yang memenuhi kriteria untuk dapat diskon.
+                cout<<"Total biaya: Rp"<< diskon<<endl;
+                cout<<"(Diskon 50% dari Rp"<<biaya<<" karena berat > 10kg)";}// dibedakan supaya variabel harga sebelum diskon tetap ada dan dapat dilihat.
+            else {cout<<"Total biaya: Rp"<< biaya;}//jika tidak masuk kriteria diskon variabel biaya akan tetap jadi variabel utama.
+            break;
+        case 2:cout<<"Ekspedisi yang dipilih: SiCepat\n";
+            if(tujuan==1){
+            biaya=8000*berat;}
+            else if(tujuan==2){
+                biaya=18000*berat;}
+            else{cout<<"Input anda salah!";}
+            if(berat>15){
+                biaya=biaya*0;
+                cout<<"Total biaya: Rp"<< biaya<<endl;
+                cout<<"(Gratis ongkir karena berat barang lebih dari 15kg)";}//penjelasan kurang lebih sama seperti case 1(JNE).
+            else {cout<<"Total biaya: Rp"<< biaya;}
+            break;
+        case 3:cout<<"Ekspedisi yang dipilih: Gojek\n";
+            biaya=12000*berat;
+            if(tujuan==1){
+                if(berat>5){
+                    diskon=biaya-10000;
+                    cout<<"Total biaya: Rp"<<diskon<<endl;
+                    cout<<"(Diskon Rp10.000 dari Rp"<<biaya<<" karena berat > 5kg)";}}
+            else cout <<"!Gojek hanya menerima pengiriman di dalam kota!";//antisipasi jika user memasukkan pilihan input selain dalam kota.
+                break;
+        default:cout<<"Input salah!";}
+}    
